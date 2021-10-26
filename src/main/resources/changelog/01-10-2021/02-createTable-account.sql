4898b24327b0b932ca2de8c051faf7882c6b0882
TaranCorp <jakubtaran1704@gmail.com>
2021-10-26 15:44:17 +0200

    Add table for Account

--- src/main/resources/changelog/01-10-2021/02-createTable-account.sql
+++ src/main/resources/changelog/01-10-2021/02-createTable-account.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset tarancorp:1
 
-create table account (
+create table if not exists account (
     id SERIAL PRIMARY KEY NOT NULL,
     login varchar(50) NOT NULL,
     password varchar(50) NOT NULL,
