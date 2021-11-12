3b247e15a4aa2e80e613ba0f7841fa677b80daef
TaranCorp <jakubtaran1704@gmail.com>
2021-11-12 07:17:36 +0100

    Added oauth2 sso id working with github

--- src/main/java/com/example/management/config/WebSecurityConfig.java
+++ src/main/java/com/example/management/config/WebSecurityConfig.java
@@ -1,24 +1,26 @@
 package com.example.management.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements ApplicationContextAware {
+@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+// @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/swagger-ui/#/")
+//                .authenticated();
+//    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-    }
-
-    protected void registerAuthentication(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-        authManagerBuilder
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("ADMIN");
-    }
 }
